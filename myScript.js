b342cb157240ac705c729e7a2381b84b283f2473
A.Westwood <personal.email.aw1@gmail.com>
2023-10-16 14:50:20 +0100

    6. write code for select size button

--- myScript.js
+++ myScript.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(32);
+    getSize();
     console.log("hi");
 })
 
@@ -13,5 +14,19 @@ function createBoard(size){
     for (let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
         board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function getSize(){
+    let input = prompt("Size of the board? 1-100");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.textContent = "Please provide a number";
+    }
+    else if(input < 0 || input > 100) {
+        message.textContent = "Provide a number between 1 and 100";
+    }
+    else {
+        message.textContent = "Draw!!";
     }
 }
