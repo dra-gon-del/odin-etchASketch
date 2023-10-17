3c7bba3a2c1c225f340bfc045e633007fb3db470
A.Westwood <personal.email.aw1@gmail.com>
2023-10-17 16:51:18 +0100

    8. add click function for drawing

--- myScript.js
+++ myScript.js
@@ -1,7 +1,24 @@
 let color = "black";
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function(e){
+
+        if(e.target.tagName != "BUTTON") {
+            click = !click
+            let draw = document.querySelector("#draw")
+            if(click){
+                draw.textContent = "Drawing"
+            }
+            else{
+                draw.textContent = "Not Drawing"
+            }
+        }
+
+    })
+
     let btnPopup = document.querySelector('#popup');
     btnPopup.addEventListener("click", function(){
         let size = getSize();
@@ -39,11 +56,13 @@ function getSize(){
 }
 
 function colorDiv(){
-    if(color == "random"){
-        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
-    }
-    else{
-        this.style.backgroundColor = 'black'
+    if(click){
+        if(color == "random"){
+            this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
+        }
+        else{
+            this.style.backgroundColor = 'black'
+        }
     }
 }
 
